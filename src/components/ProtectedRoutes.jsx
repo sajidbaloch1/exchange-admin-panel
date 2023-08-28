ec7722095a8bc9dd8dabc580380d472418896616
Badal <badal@rethinksoft.com>
2023-08-28 17:12:22 +0400

    updated code

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,33 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-import { Navigate, Outlet, useLocation } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const ProtectedRoutes = ({ allowedRoles }) => {
+  const location = useLocation();
+  let token = localStorage.getItem("jws_token");
+  const { role } = JSON.parse(localStorage.getItem("user_info")) || {};
+  console.log(allowedRoles);
+  console.log(token);
 
-    const location = useLocation();
-    let token = localStorage.getItem('jws_token');
-    const { role } = JSON.parse(localStorage.getItem('user_info')) || {};
+  useEffect(() => {}, [role, allowedRoles]);
 
-    useEffect(() => {
+  if (allowedRoles && token) {
+    return <Outlet />;
+  } else if (token) {
+    return <Navigate to="/errorpage404" state={{ from: location }} replace />;
+  } else {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
-    }, [role, allowedRoles]);
-
-    if (allowedRoles && token) {
-        return <Outlet />;
-    } else if (token) {
-        return <Navigate to="/errorpage404" state={{ from: location }} replace />;
-    } else {
-        return <Navigate to="/login" state={{ from: location }} replace />;
-    }
-
-    // const isRoleAllowed = allowedRoles && allowedRoles.includes(role);
-    // if (isRoleAllowed) {
-    //     return <Outlet />;
-    // } else if (token) {
-    //     return <Navigate to="/errorpage404" state={{ from: location }} replace />;
-    // } else {
-    //     return <Navigate to="/login" state={{ from: location }} replace />;
-    // }
-}
+  // const isRoleAllowed = allowedRoles && allowedRoles.includes(role);
+  // if (isRoleAllowed) {
+  //     return <Outlet />;
+  // } else if (token) {
+  //     return <Navigate to="/errorpage404" state={{ from: location }} replace />;
+  // } else {
+  //     return <Navigate to="/login" state={{ from: location }} replace />;
+  // }
+};
 
 export default ProtectedRoutes;
